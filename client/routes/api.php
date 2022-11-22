4aab1978c65ca1de02e292ba3d29b33f87531105
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-11-22 20:22:40 -0300

    :route: add middlwere

--- client/routes/api.php
+++ client/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PatenteController;
 use App\Http\Controllers\SoftwareController;
 use App\Http\Controllers\ServicoController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,26 +18,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Auth user
+Route::post('/login', [AuthController::class, 'login']);
+
 // Patente routes
 Route::get('/patentes', [PatenteController::class, 'index']);
 Route::get('/patentes/{id}', [PatenteController::class, 'show']);
-Route::post('/patentes', [PatenteController::class, 'store']);
-Route::put('/patentes/{id}', [PatenteController::class, 'update']);
-Route::delete('/patentes/{id}', [PatenteController::class, 'destroy']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/patentes', [PatenteController::class, 'store']);
+    Route::put('/patentes/{id}', [PatenteController::class, 'update']);
+    Route::delete('/patentes/{id}', [PatenteController::class, 'destroy']);
+});
 
 // Software routes
 Route::get('/softwares', [SoftwareController::class, 'index']);
 Route::get('/softwares/{id}', [SoftwareController::class, 'show']);
-Route::post('/softwares', [SoftwareController::class, 'store']);
-Route::put('/softwares/{id}', [SoftwareController::class, 'update']);
-Route::delete('/softwares/{id}', [SoftwareController::class, 'destroy']);
+
+Route::group(['middlwere' => ['auth:sanctum']], function () {
+  Route::post('/softwares', [SoftwareController::class, 'store']);
+  Route::put('/softwares/{id}', [SoftwareController::class, 'update']);
+  Route::delete('/softwares/{id}', [SoftwareController::class, 'destroy']);
+});
 
 // Servicos routes
 Route::get('/servicos', [ServicoController::class, 'index']);
 Route::get('/servicos/{id}', [ServicoController::class, 'show']);
-Route::post('/servicos', [ServicoController::class, 'store']);
-Route::put('/servicos/{id}', [ServicoController::class, 'update']);
-Route::delete('/servicos/{id}', [ServicoController::class, 'destroy']);
+
+Route::group(['middlwere' => ['auth:sanctum']], function () {
+  Route::post('/servicos', [ServicoController::class, 'store']);
+  Route::put('/servicos/{id}', [ServicoController::class, 'update']);
+  Route::delete('/servicos/{id}', [ServicoController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
