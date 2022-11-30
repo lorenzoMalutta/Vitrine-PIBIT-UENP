57f6bc870f1e91144eaf2a1d4c13f749faca0997
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-11-30 19:17:37 -0300

    :lipstick: feat home

--- client/src/view/home/index.tsx
+++ client/src/view/home/index.tsx
@@ -1,7 +1,28 @@
 export function Home() {
   return (
-    <div>
-      <h1 className="text-3xl p-10 bg-slate-400 font-bold underline">Home</h1>
-    </div>
+    <section className="grid">
+      <div className="grid grid-cols-5 m-5 gap-5 p-4 ">
+        <div className="flex flex-wrap justify-center bg-white shadow-2xl hover:bg-[#D7D7D7] duration-700 rounded-md">
+          <h2 >Patentes</h2>
+          <p>Inovação e Tecnologias</p>
+        </div>
+        <div className="flex flex-wrap justify-center bg-white shadow-2xl hover:bg-[#D7D7D7] duration-700 rounded-md">
+          <h2>Softwares</h2>
+          <p>Inovação e Tecnologias</p>
+        </div>
+        <div className="flex flex-wrap justify-center bg-white shadow-2xl hover:bg-[#D7D7D7] duration-700 rounded-md">
+          <h2>Serviços</h2>
+          <p>Inovação e Tecnologias</p>
+        </div>
+        <div className="flex flex-wrap justify-center bg-white shadow-2xl hover:bg-[#D7D7D7] duration-700 rounded-md">
+          <h2>Startups</h2>
+          <p>Inovação e Tecnologias</p>
+        </div>
+        <div className="flex flex-wrap justify-center bg-white shadow-2xl hover:bg-[#D7D7D7] duration-700 rounded-md">
+          <h2>Pesquisas</h2>
+          <p>Inovação e Tecnologias</p>
+        </div>
+      </div>
+    </section>
   )
 }
