a4ac9e14cebd0ed7efc4f929bb5269a7ff20b26e
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-12-01 15:24:39 -0300

    :lipstick: feat vitrine patente

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './view/home/index'
+import { Patente } from './view/vitrine/patente'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/patentes" element={<Patente />} />
     </Routes>
   )
 }
--- client/src/view/vitrine/patente/index.tsx
+++ client/src/view/vitrine/patente/index.tsx
@@ -1,7 +1,54 @@
+import { Cards } from "../../../components/cards";
+import { Filter } from "../../../components/filter";
+
 export function Patente() {
   return (
-    <div>
-      <h1>Patente</h1>
-    </div>
+    <>
+      <div className="flex m-10">
+        <h1>Vitrine Patentes</h1>
+      </div>
+      <div className="grid justify-center pb-20">
+        <div className="grid grid-cols-4 max-w-fit gap-14 ">
+          <Filter
+            filter="Vitrine de Patentes"
+            catergorias={["categoria1", "categoria2", "categoria3"]}
+            setFilter={() => { }}
+          />
+          <Cards
+            title="Patentes"
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+            link="/patentes"
+            img="src\assets\card-top.jpg"
+            tags={
+              [
+                1, 3, 4, 5
+              ]
+            }
+          />
+          <Cards
+            title="Patentes"
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+            link="/patentes"
+            img="src\assets\card-top.jpg"
+            tags={
+              [
+                1, 3, 4, 5
+              ]
+            }
+          />
+          <Cards
+            title="Patentes"
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+            link="/patentes"
+            img="src\assets\card-top.jpg"
+            tags={
+              [
+                1, 3, 4, 5
+              ]
+            }
+          />
+        </div>
+      </div>
+    </>
   )
 }
