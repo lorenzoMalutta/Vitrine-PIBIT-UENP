3aefc8621b9d2eed0126f3733cb75579c45a9d8a
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-12-03 18:40:24 -0300

    :recycle: refactor component cards

--- client/src/components/cards/index.tsx
+++ client/src/components/cards/index.tsx
@@ -1,33 +1,35 @@
 import { Link } from "react-router-dom";
 
 interface CardProps {
-  title: string;
-  description: string;
-  link: string;
-  img: string;
-  tags: any[];
+  nome: string;
+  sinopse: string;
+  img?: string;
+  palavraChave: any[];
+  id: number;
+  areaCientifica: string;
+  areaEconomica: string;
 }
 
-export function Cards( { title, description, link, img, tags }: CardProps ) {
+export function Cards({ palavraChave, nome, sinopse, img, areaCientifica, areaEconomica, id }: CardProps) {
   return (
-    
+
     <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white">
-      <Link to={link}>
+      <Link to="/">
       <img className="w-full" src={img} alt="Sunset in the mountains" />
       <div className="px-6 py-4">
-        <div className="font-bold text-gray-700 text-xl mb-2">{title}</div>
+        <div className="font-bold text-gray-700 text-xl mb-2">{nome}</div>
         <p className="text-gray-700 text-base">
-          {description}
+          {sinopse}
         </p>
       </div>
-      
-      <div className="px-6 pt-4 pb-2">
-        {tags.map((tag) => (
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
-        ))}
+
+      <div className="grid grid-cols-2 px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{areaCientifica}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{areaEconomica}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{palavraChave}</span>
       </div>
       </Link>
     </div>
-    
+
   )
 }
