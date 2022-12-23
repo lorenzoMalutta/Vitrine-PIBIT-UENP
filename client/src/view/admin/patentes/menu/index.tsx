c498da759b1acd0fe2570180931afe835a8d96d8
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-12-23 20:13:39 -0300

    :lipstick: feat menu admin

--- client/src/view/admin/patentes/menu/index.tsx
+++ client/src/view/admin/patentes/menu/index.tsx
@@ -1,23 +1,36 @@
 import { Link } from "react-router-dom";
-
 export function AdminPatente() {
   return (
-    <section>
-      <div>
-        <h1>patentes</h1>
-        <ul>
-          <li>
-            <Link to="/admin/patentes/cadastrar">
-              Cadastrar Patente
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/patentes/editar-delete">
-              Deletar ou Editar Patente
-            </Link>
-          </li>
-        </ul>
+    <section className="grid pb-20 pt-20 justify-center">
+      <h1>Administrador de patentes</h1>
+      <div className="absolute top-56 left-72 -z-10">
+        <img
+          src="..\src\assets\dashboard\quadrado0.png"
+          alt=""
+        />
+        <img className="absolute top-56 left-56 -z-10"
+          src="..\src\assets\dashboard\quadrado1.png"
+          alt=""
+        />
       </div>
+      <ul className="grid grid-cols-2 w-fit items-center gap-11">
+        <li className="w-fit">
+          <div className="bg-white max-w-[230px] p-2 rounded shadow-md text-center">
+            <Link to="/admin/patentes/cadastrar">
+              <img src="..\src\assets\dashboard\img1.svg" alt="" />
+              <h4>Cadastrar Patentes</h4>
+            </Link>
+          </div>
+        </li>
+        <li className="w-fit">
+          <div className="bg-white max-w-[230px] p-2 rounded shadow-md text-center">
+            <Link to="/admin/patentes/editar-delete">
+              <img src="..\src\assets\dashboard\img2.svg" alt="" />
+              <h4>Deletar ou Editar Patente</h4>
+            </Link>
+          </div>
+        </li>
+      </ul>
     </section>
   )
 }
