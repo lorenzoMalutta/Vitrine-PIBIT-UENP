f2cc43b18a004228aca33cf1cdcafc273f53bb4c
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-12-25 12:19:14 -0300

    :iphone: feat menu hamburguer

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -7,9 +7,15 @@ import { AiOutlineFileSearch } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { IoLogInOutline } from "react-icons/io5";
 import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
 import logo from "../../assets/logo-aitec.png";
+import { useState } from "react";
 
 export function Header() {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const onClick = () => setIsExpanded(!isExpanded);
+
   return (
     <header className="grid h-max">
       <div className="grid grid-cols-2 p-2 bg-[#26448B]">
@@ -17,6 +23,12 @@ export function Header() {
         <ul className="inline-flex items-center justify-end gap-2 text-2xl text-white ">
           <li>
             <Link to="/"> <AiOutlineUser /> </Link>
+          </li>
+          <li onClick={onClick} className={isExpanded
+            ? "block"
+            : "sm:hidden"
+          }>
+            <AiOutlineMenu />
           </li>
           <li>
             <Link to="/login"> <IoLogInOutline /> </Link>
@@ -28,24 +40,24 @@ export function Header() {
       </div>
       <div className="bg-white h-3 shadow-2x1" />
       <div className="bg-[#5175B3] h-10 shadow" />
-      <div className="sm:flex sm:justify-center text-center text-white bg-white drop-shadow-2xl sm:ml-40 sm:mr-40 rounded-md ">
-          <ul className="sm:inline-flex grid sm:items-center justify-center gap-6 p-3 rounded-sm text-gray-700">
-            <li className="inline-flex items-center gap-1">
-              <IoMdPaper /><Link to="/patentes">Patentes</Link>
-            </li>
-            <li className="inline-flex items-center gap-1">
-              <IoHardwareChipOutline /><Link to="/softwares">Softwares</Link>
-            </li>
-            <li className="inline-flex items-center gap-1">
-              <BsBox /><Link to="/servicos">Serviços</Link>
-            </li>
-            <li className="inline-flex items-center gap-1">
-              <BiRocket /><Link to="/startups">Startups</Link>
-            </li>
-            <li className="inline-flex items-center gap-1">
-              <AiOutlineFileSearch /><Link to="/pesquisas">Pesquisas</Link>
-            </li>
-          </ul>
+      <div className={ !isExpanded ? "hidden" : "sm:flex sm:justify-center text-center text-white bg-white drop-shadow-2xl sm:ml-40 sm:mr-40 rounded-md" }>
+        <ul className="sm:inline-flex grid sm:items-center justify-center gap-6 p-3 rounded-sm text-gray-700">
+          <li className="inline-flex items-center gap-1">
+            <IoMdPaper /><Link to="/patentes">Patentes</Link>
+          </li>
+          <li className="inline-flex items-center gap-1">
+            <IoHardwareChipOutline /><Link to="/softwares">Softwares</Link>
+          </li>
+          <li className="inline-flex items-center gap-1">
+            <BsBox /><Link to="/servicos">Serviços</Link>
+          </li>
+          <li className="inline-flex items-center gap-1">
+            <BiRocket /><Link to="/startups">Startups</Link>
+          </li>
+          <li className="inline-flex items-center gap-1">
+            <AiOutlineFileSearch /><Link to="/pesquisas">Pesquisas</Link>
+          </li>
+        </ul>
       </div>
     </header>
   )
