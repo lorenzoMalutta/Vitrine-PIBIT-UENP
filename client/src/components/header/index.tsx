7e45523f6b83dacc27ddd1d892c8702cda4617d1
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-01-12 11:23:13 -0300

    :lipstick: feat logout

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoHardwareChipOutline } from "react-icons/io5";
 import { IoMdPaper } from "react-icons/io";
 import { BsBox } from "react-icons/bs";
@@ -10,25 +10,50 @@ import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineMenu } from "react-icons/ai";
 import logo from "../../assets/logo-aitec.png";
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import api from "../../services/api";
 
 export function Header() {
 
-  const [isExpanded, setIsExpanded] = useState(false);
-  const onClick = () => setIsExpanded(!isExpanded);
+  const [expanded, setExpanded] = useState(false);
+  const menuMobile = () => setExpanded(!expanded);
+  const navigate = useNavigate();
+  console.log(localStorage.getItem('token'))
+  const logout = () => {
+    api.post("/logout", null, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': "Bearer " + localStorage.getItem('token')
+      }
+    })
+    if (localStorage.getItem('authenticating') == 'true') {
+      localStorage.removeItem('token')
+      localStorage.setItem('authenticating', 'false')
+      toast.success('Deslogado com sucesso!')
+      setTimeout(() => {
+        navigate('/')
+      }, 100)
+    } else {
+      console.log('não logado')
+      navigate('/login')
+    }
+  }
 
   return (
     <header className="grid h-max">
+      <ToastContainer />
       <div className="grid grid-cols-2 p-2 bg-[#26448B]">
         <img className="max-w-[200px] sm:max-w-sm " src={logo} alt="" />
         <ul className="inline-flex items-center justify-end gap-2 text-2xl text-white ">
           <li>
             <Link to="/"> <AiOutlineUser /> </Link>
           </li>
-          <li onClick={onClick} className="sm:hidden">
+          <li onClick={menuMobile} className="sm:hidden cursor-pointer">
             <AiOutlineMenu />
           </li>
-          <li>
-            <Link to="/login"> <IoLogInOutline /> </Link>
+          <li onClick={logout} className="cursor-pointer">
+            <IoLogInOutline />
           </li>
           <li>
             <Link to="/"> <AiOutlineHome /> </Link>
@@ -37,7 +62,7 @@ export function Header() {
       </div>
       <div className="bg-white h-3 shadow-2x1" />
       <div className="bg-[#5175B3] h-10 shadow" />
-      <div className={ !isExpanded ? "sm:flex sm:justify-center text-center text-white bg-white drop-shadow-2xl sm:ml-40 sm:mr-40 rounded-md" : "hidden" }>
+      <div className={!expanded ? "sm:flex sm:justify-center text-center text-white bg-white drop-shadow-2xl sm:ml-40 sm:mr-40 rounded-md" : "hidden"}>
         <ul className="sm:inline-flex grid sm:items-center justify-center gap-6 p-3 rounded-sm text-gray-700">
           <li className="inline-flex items-center gap-1">
             <IoMdPaper /><Link to="/patentes">Patentes</Link>
