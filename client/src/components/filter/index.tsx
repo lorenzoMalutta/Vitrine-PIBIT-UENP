2e7a27b28153b3773ccc06285c82c9a10aa408dc
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-01-15 20:32:07 -0300

    :lipstick: feat component filter funcional com api
    TO DO: terminar os if else para mostrar as opções no filter e colocar useEffect no filter

--- client/src/components/filter/index.tsx
+++ client/src/components/filter/index.tsx
@@ -9,7 +9,16 @@ interface PropsFilter {
   type: "patentes" | "softwares" | "pesquisas" | "servicos" | "startups";
 }
 
+
 export function Filter({ filter, setFilter, type }: PropsFilter) {
+  const [valorBusca, setValorBusca] = useState('');
+
+
+  api.get(`/patentes/search/${valorBusca}`).then(response => {
+    setFilter(response.data);
+  })
+
+
   if (type === "patentes") {
     const [optionPalavraChave, setOptionPalavraChave] = useState<Iareas[]>([]);
     useEffect(() => {
@@ -17,17 +26,20 @@ export function Filter({ filter, setFilter, type }: PropsFilter) {
         setOptionPalavraChave(response.data);
       })
     }, [])
+
     return (
       <div className="bg-white flex flex-col h-max p-4 shadow-xl rounded-md">
-        <h4 className="text-gray-700 font-medium">{filter}</h4>
-        <input type="text" placeholder="Pesquise..." onChange={(e) => setFilter(e.target.value)} />
-        {optionPalavraChave.map((optionPalavraChave) => (
-          <div className="flex gap-1 items-center p-1" key={optionPalavraChave.denominacao}>
-            <input className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" />
-            <label>{optionPalavraChave.denominacao}</label>
-          </div>
-        ))}
-        <button type="submit" className="flex justify-center">Filtrar</button>
+        <form>
+          <h4 className="text-gray-700 font-medium">{filter}</h4>
+          <input type="text" placeholder="Pesquise..." onChange={(e) => setValorBusca(e.target.value)} />
+          {optionPalavraChave.map((optionPalavraChave) => (
+            <div className="flex gap-1 items-center p-1" key={optionPalavraChave.denominacao}>
+              <input onChange={(e) => setValorBusca(e.target.value)} value={optionPalavraChave.denominacao} className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" />
+              <label>{optionPalavraChave.denominacao}</label>
+            </div>
+          ))}
+          <button type="submit" className="flex justify-center">Filtrar</button>
+        </form>
       </div>
     )
   }
--- client/src/view/vitrine/patente/index.tsx
+++ client/src/view/vitrine/patente/index.tsx
@@ -7,6 +7,15 @@ import { Title } from "../../../components/title";
 interface Iareas {
   denominacao: string;
 }
+interface Ifiltro {
+  nome: string;
+  sinopse: string;
+  palavra_chave: any;
+  id: number;
+  tipo: string;
+  area_cientifica: string;
+  area_economica: string;
+}
 interface IHome {
   nome: string;
   sinopse: string;
@@ -19,8 +28,13 @@ interface IHome {
 }
 
 export function Patente() {
+  const [filter, setFilter] = useState<Ifiltro[]>([])
   const [patente, setPatente] = useState<IHome[]>([]);
   const [optionPalavraChave, setOptionPalavraChave] = useState<Iareas[]>([]);
+
+  const filtro = (childrenData: any) => {
+    setFilter(childrenData);
+  }
 
   useEffect(() => {
     api.get('/palavraChave').then(response => {
@@ -32,25 +46,62 @@ export function Patente() {
       setPatente(response.data);
     })
   }, [])
-
-  return (
-    <section >
+  console.log(filter)
+  if (filter.map((filtro) => filtro.palavra_chave == 'Fertilizantes')) {
+    return (
+      <section >
+        <div className="mx-auto text-center">
+          <Title
+            titulo="Vitrine de Patentes"
+            subtitulo="Tecnologias e Inovação "
+          />
+        </div>
+        <div className="flex">
+          <div className="m-10">
+            <Filter
+              filter="Vitrine de Patentes"
+              setFilter={filtro}
+              type="patentes"
+            />
+          </div>
+          <div className="grid justify-center mb-20 mt-10">
+            <div className="grid grid-cols-3 gap-5">
+              {patente.map((patente) => (
+                <Cards
+                  type="patentes"
+                  image={patente.image}
+                  nome={patente.nome}
+                  sinopse={patente.sinopse}
+                  palavraChave={patente.palavra_chave}
+                  id={patente.id}
+                  areaCientifica={patente.area_cientifica}
+                  areaEconomica={patente.area_economica}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  } else if (filter) {
+    return (
+      <section >
       <div className="mx-auto text-center">
-        <Title 
-        titulo="Vitrine de Patentes"
-        subtitulo="Tecnologias e Inovação "
+        <Title
+          titulo="Vitrine de Patentes"
+          subtitulo="Tecnologias e Inovação "
         />
       </div>
       <div className="flex">
         <div className="m-10">
           <Filter
             filter="Vitrine de Patentes"
-            setFilter={() => { }}
+            setFilter={filtro}
             type="patentes"
           />
         </div>
         <div className="grid justify-center mb-20 mt-10">
-          <div className="grid grid-cols-3 max-w-fit gap-5">
+          <div className="grid grid-cols-3 gap-5">
             {patente.map((patente) => (
               <Cards
                 type="patentes"
@@ -67,5 +118,6 @@ export function Patente() {
         </div>
       </div>
     </section>
-  )
+    )
+  }
 }
