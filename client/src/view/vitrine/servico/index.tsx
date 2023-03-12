11fcb4311ed239ed302f533c0a55960fbdfc226d
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-03-11 22:35:46 -0300

    add vitrine to servico

--- client/src/view/vitrine/servico/index.tsx
+++ client/src/view/vitrine/servico/index.tsx
@@ -1,7 +1,135 @@
+import { Children, useEffect, useState } from "react";
+import { Cards } from "../../../components/cards";
+import { Filter } from "../../../components/filter";
+import api from "../../../services/api";
+import { Title } from "../../../components/title";
+
+interface Iareas {
+    denominacao: string;
+}
+interface Ifiltro {
+    nome: string;
+    sinopse: string;
+    palavra_chave: string;
+    id: number;
+    tipo: string;
+    area_cientifica: string;
+    area_economica: string;
+}
+interface IHome {
+    nome: string;
+    sinopse: string;
+    palavra_chave: string;
+    id: number;
+    tipo: string;
+    area_cientifica: string;
+    area_economica: string;
+    image: string;
+}
+
 export function Servico() {
-  return (
-    <div>
-      <h1>Servico</h1>
-    </div>
-  )
+    const [filter, setFilter] = useState<Ifiltro[]>([])
+    const [servico, setServico] = useState<IHome[]>([]);
+    const [busca, setBusca] = useState<string>("");
+    const [optionPalavraChave, setOptionPalavraChave] = useState<Iareas[]>([]);
+
+    const filtro = (childrenData: any) => {
+        setFilter(childrenData)
+    }
+    // console.log(filter)
+
+    const buscar = (childrenData: any) => {
+        setBusca(childrenData)
+    }
+    console.log(busca)
+
+    useEffect(() => {
+        api.get('/palavraChave').then(response => {
+            setOptionPalavraChave(response.data);
+        })
+    }, [])
+    useEffect(() => {
+        api.get('/servicos').then(response => {
+            setServico(response.data);
+        })
+    }, [])
+    if (busca == "todos") {
+        return (
+            <section className="h-fit">
+                <div className="mx-auto text-center">
+                    <Title
+                        titulo="Vitrine de Serviços"
+                        subtitulo="Prestação de Serviços"
+                    />
+                </div>
+                <div className="flex h-screen">
+                    <div className="m-10">
+                        <Filter
+                            nomeFiltro="Serviços"
+                            setFilter={filtro}
+                            type="servicos"
+                            setBusca={buscar}
+                        />
+                    </div>
+                    <div className="grid justify-center mb-20 mt-10" >
+                        <div className="grid grid-cols-4 gap-5 max-h-56">
+                            {servico.map((servico) => (
+                                <Cards
+                                    type="servicos"
+                                    image={servico.image}
+                                    nome={servico.nome}
+                                    sinopse={servico.sinopse}
+                                    palavraChave={servico.palavra_chave}
+                                    id={servico.id}
+                                    areaCientifica={servico.area_cientifica}
+                                    areaEconomica={servico.area_economica}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    } else if (filter.map((filtro) => filtro.palavra_chave == busca)) {
+        return (
+            <section >
+                <div className="mx-auto text-center">
+                    <Title
+                        titulo="Vitrine de Patentes"
+                        subtitulo="Tecnologias e Inovação "
+                    />
+                </div>
+                <div className="flex">
+                    <div className="m-10">
+                        <Filter
+                            nomeFiltro="Vitrine de Serviços"
+                            setFilter={filtro}
+                            type="servicos"
+                            setBusca={buscar}
+                        />
+                    </div>
+                    <div className="grid justify-center mb-20 mt-10">
+                        <div className="grid grid-cols-3 gap-5 max-h-56">
+                            {servico.map((servico) => {
+                                if (busca == servico.palavra_chave) {
+                                    return (
+                                        <Cards
+                                            type="patentes"
+                                            image={servico.image}
+                                            nome={servico.nome}
+                                            sinopse={servico.sinopse}
+                                            palavraChave={servico.palavra_chave}
+                                            id={servico.id}
+                                            areaCientifica={servico.area_cientifica}
+                                            areaEconomica={servico.area_economica}
+                                        />
+                                    )
+                                }
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
